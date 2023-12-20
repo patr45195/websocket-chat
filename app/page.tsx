9531a9f3369b0453165a5d157ce6ef9d49d59102
patr45195 <patr45195@gmail.com>
2023-12-20 12:08:33 +0300

    +

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,74 @@
 "use client";
 
 import React, { FormEvent } from "react";
-import Button from "@mui/material/Button";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Button } from "./ui/button";
 
 export default function Home() {
-  const [user, setUser] = React.useState("");
-
   const router = useRouter();
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    let { data } = await axios.get<boolean>(
-      "http://localhost:5000/freeUserName",
-      {
-        params: { userName: user.trim() },
-      }
-    );
-
-    const canCreateUser = data;
-
-    if (canCreateUser) {
-      localStorage.setItem("user", user.trim());
-      router.push("/chat");
-    } else {
-      setUser("");
-      alert("Nickname is busy, come up with another nickname.");
-    }
-  };
 
   return (
     <main className="flex items-center justify-center h-screen">
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <div className="mb-2">Enter chat</div>
-        <div className="mb-2">
-          <label htmlFor="userName"></label>
-          <input
-            id="userName"
-            placeholder="Input your name..."
-            value={user}
-            onChange={(e) => setUser(e.target.value)}
-            className="border p-2 rounded-md"
-          />
-        </div>
-        <Button type="submit" variant="contained">Go</Button>
-      </form>
+      <Formik
+        initialValues={{ name: "" }}
+        onSubmit={async (values) => {
+          let { data } = await axios.get<boolean>(
+            "http://localhost:5000/freeUserName",
+            {
+              params: { userName: values.name.trim() },
+            }
+          );
+
+          const canCreateUser = data;
+
+          if (canCreateUser) {
+            localStorage.setItem("user", values.name.trim());
+            router.push("/chat");
+          } else {
+            values.name = "";
+            alert("Nickname is busy, come up with another nickname.");
+          }
+        }}
+        validationSchema={Yup.object().shape({
+          name: Yup.string()
+            .required("Required")
+            .min(3, "Minimum length 3 characters"),
+        })}
+      >
+        {(props) => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleSubmit,
+          } = props;
+          return (
+            <form onSubmit={handleSubmit} className="flex flex-col items-center">
+              <label htmlFor="name"></label>
+              <input
+                className="border p-2 rounded-md"
+                id="name"
+                placeholder="Enter your name"
+                type="text"
+                value={values.name}
+                onChange={handleChange}
+              />
+              {errors.name && touched.name && (
+                <div className="text-red-500 text-sm">{errors.name}</div>
+              )}
+              <Button type="submit" disabled={isSubmitting}>
+                Go
+              </Button>
+            </form>
+          );
+        }}
+      </Formik>
     </main>
   );
 }
